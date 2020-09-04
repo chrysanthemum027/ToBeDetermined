7b9584beae5d36bc8ab470434aacf2248ac4ed4f
secrashi <56053777+secrashi@users.noreply.github.com>
2020-09-04 18:41:29 +0530

    Add files via upload

---
+++ mental-health-master/keys.js
@@ -0,0 +1,3 @@
+module.exports={
+    MONGOURI : 'mongodb+srv://savi:sham1311BHAVI@cluster0-46c53.mongodb.net/test?retryWrites=true&w=majority'
+}
---
+++ mental-health-master/server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const expressLayouts = require("express-ejs-layouts");
+const mongoose = require('mongoose');
+const nodemailer = require("nodemailer")
+const {MONGOURI} = require('./keys')
+
+mongoose.connect(MONGOURI,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+})
+mongoose.connection.on('connected',()=>{
+    console.log("Connected DATABASE")
+})
+mongoose.connection.on('error',(err)=>{
+    console.log("err connecting",err)
+})
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(session({
+  secret: 'seCReT',
+  cookie: { maxAge: 3600000 },
+  saveUninitialized: false
+}));
+
+app.use(express.static('public'));
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+
+
+
+//app.use('/', require('./routes/app.js'));
+
+
+app.get('/home', (req, res) => {
+  res.status(200);
+  res.render("home");
+});
+
+app.get('*', (req, res) => {
+  res.status(404);
+  res.redirect("/home");
+});
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
